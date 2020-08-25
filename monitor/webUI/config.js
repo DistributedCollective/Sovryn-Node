edef5fa06e20a228e24e135729a6e88f27e4eee3
Jamie Madrox <jamie.madrox779@gmail.com>
2020-08-25 09:04:48 +0000

    health monitor completed

---
+++ monitor/webUI/config.js
@@ -0,0 +1,11 @@
+/**
+ * PM2 monitor servers
+ */
+
+const servers = {
+    'liquidation watcher': [{
+        ip: '18.138.223.132',
+        port: 3001,
+        show: true
+    }
+]};
--- start.js
+++ start.js
@@ -12,17 +12,20 @@ if(process.argv[2]=="mainnet") global.conf=cMain;
 else global.conf=cTest;
 
 
-const monitor = require('pm2-server-monitor');
+const monitor = require('./monitor/index.js');
 monitor({
     name: 'liquidation-watcher',
-    port: conf.serverPort
+    port: conf.serverPort+1
 });
 
 import mainController from './controller/main';
 
 console.log("Hola. It is "+new Date(Date.now())+ ". Starting the app on "+process.argv[2]);
        
-app.use(express.static('public/dist'));
+app.use('/', express.static('public/dist'));
+
+app.use('/monitor', express.static('monitor/webUI'));
+
 
 http.listen(conf.serverPort, () => {
     console.log('listening on *:'+conf.serverPort);
