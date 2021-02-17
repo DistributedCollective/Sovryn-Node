0e9f976b6c14e56a1898c5faef3ec1ebdf8beeff
Bitcoinera <anagjordano@gmail.com>
2021-02-17 19:48:15 +0100

    add web3.eth.Contract

--- util/approval.js
+++ util/approval.js
@@ -3,11 +3,15 @@
  * These approvals should be executed for every liquidator and arbitrage wallet once in order for liquidation or interacting with the 
  * Swap contract can work flawlessly
 */
-import conf from '../config/config';
+import Web3 from 'web3';
 import C from '../controller/contract';
 import W from '../secrets/accounts';
 import tokensDictionary from '../config/tokensDictionary.json'
+import abiTestToken from '../config/abiTestToken';
+import abiRBTCWrapperProxy from '../config/abiRBTCWrapperProxy';
+import conf from '../config/config';
 
+const web3 = new Web3(conf.nodeProvider);
 const tokenContracts = Object.keys(tokensDictionary[conf.network])
 const amount = C.web3.utils.toWei("1000000000", 'ether');
 
@@ -29,6 +33,9 @@ async function approveLiquidatorWallets() {
         let approved;
 
         for (let tokenContract in tokenContracts) {
+            if (tokenContract !== C.contractTokenRBTC) tokenContract = new web3.eth.Contract(abiRBTCWrapperProxy, tokenContract.toLowerCase());
+            else tokenContract = new web3.eth.Contract(abiTestToken, tokenContract.toLowerCase());
+
             //should approve the Sovryn contract to spend the token for the main account
             console.log("approving " + from + " " + conf.sovrynProtocolAdr + " for " + amount)
             approved = await C.approveToken(tokenContract, from, conf.sovrynProtocolAdr, amount);
@@ -66,12 +73,13 @@ async function approveArbitrageWallets() {
     let approved;
 
     for (let tokenContract in tokenContracts) {
+        tokenContract = new web3.eth.Contract(abiTestToken, tokenContract.toLowerCase());
         //should approve the swap network contract (conf.swapsImpl) to spend the token for the main account
         console.log("approving " + from + " " + conf.sovrynProtocolAdr + " for " + amount)
         approved = await C.approveToken(tokenContract, from, conf.swapsImpl, amount);
         console.log(approved);
 
-        if (tokenContracts !== C.contractTokenRBTC) {
+        if (tokenContract !== C.contractTokenRBTC) {
             //should approve the wRBTC wrapper contract to spend the token for the main account
             approved = await C.approveToken(tokenContract, from, conf.wRbtcWrapper, amount);
             console.log(approved);
