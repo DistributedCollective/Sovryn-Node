6281a06cea50d13266058d7568a81039793bdc9f
Bitcoinera <anagjordano@gmail.com>
2021-04-09 20:26:39 +0200

    fix rollover collateral token is undefined

--- controller/rollover.js
+++ controller/rollover.js
@@ -71,7 +71,7 @@ class Rollover {
             C.contractSovryn.methods.rollover(pos.loanId, loanDataBytes)
                 .send({ from: wallet, gas: 2500000, gasPrice: gasPrice, nonce:nonce })
                 .then(async (tx) => {
-                    const msg = `Rollover Transaction successful: ${tx.transactionHash} \n Rolled over position ${pos.loanId} with ${tokensDictionary[conf.network][pos.collateralToken]} as collateral token`;
+                    const msg = `Rollover Transaction successful: ${tx.transactionHash} \n Rolled over position ${pos.loanId} with ${tokensDictionary[conf.network][pos.collateralToken].toLowerCase()} as collateral token`;
                     console.log(msg);
                     await common.telegramBot.sendMessage(`${conf.network}-${msg}`);
 
